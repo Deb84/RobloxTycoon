9b943f3b978181bfb04eff96bc253b2358d7b8a9
Deb <56758812+Deb84@users.noreply.github.com>
2025-05-26 16:56:48 +0200

    Create Sprint.lua

---
+++ StarterPlayer/StarterPlayerScripts/Sprint.lua
@@ -0,0 +1,20 @@
+local player = game.Players.LocalPlayer
+local ContextActionService = game:GetService("ContextActionService")
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
+
+local defaultSpeed = 16
+local sprintSpeed = 25
+
+
+local function sprint(actionName, inputState)
+	if inputState == Enum.UserInputState.Begin then
+		humanoid.WalkSpeed = sprintSpeed 
+	end
+	if inputState == Enum.UserInputState.End then
+		humanoid.WalkSpeed = defaultSpeed 
+	end
+	
+end
+
+ContextActionService:BindAction('Sprint', sprint, true, Enum.KeyCode.LeftShift)
