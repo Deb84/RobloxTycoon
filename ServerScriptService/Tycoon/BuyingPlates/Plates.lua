126166499c19806d86bde59befa2a6fd97e6fc1f
Deb <56758812+Deb84@users.noreply.github.com>
2025-05-31 15:14:20 +0200

    Update Plates.lua

--- ServerScriptService/Tycoon/BuyingPlates/Plates.lua
+++ ServerScriptService/Tycoon/BuyingPlates/Plates.lua
@@ -1,24 +1,55 @@
 local plateList = require(script.Parent.PlatesList)
 local Players = game:GetService("Players")
+local ServerScriptService = game:GetService('ServerScriptService')
 local ReplicatedStorage = game:GetService('ReplicatedStorage')
-TycoonBasePlate = workspace.Build.TycoonBaseplate
+local workspaceTeams = workspace:WaitForChild('Teams')
+local CoinsUtils = require(ServerScriptService.Leaderboard.CoinsUtils)
+
+local function EventMessage(plateConfig, plateId, baseplate, team)
+	local message = {
+		plateType = plateConfig.plateType,
+		targetId = plateConfig.targetId,
+		plateId = plateId,
+		baseplate = baseplate,
+		team = team
+	}
+	
+	local _event = nil
+	if plateConfig.targetType == 'Dropper' then
+		_event = ServerScriptService.Events.Spawn.DroppersSpawn
+	elseif plateConfig.targetType == 'Part' then
+		_event = ServerScriptService.Events.Spawn.PartsSpawn
+	elseif plateConfig.targetType == 'Conveyor' then
+		_event = ServerScriptService.Events.Spawn.ConveyorsSpawn
+	end
+	
+	
+	if _event then
+		local eventPlatePressed = ServerScriptService.Events.PlatesPressedEvent
+
+		eventPlatePressed:Fire(message)
+		_event:Fire(message)
+	end	
+end
 
 
-local function spawn_plate(plateId)
+local function spawn_plate(plateId, baseplate, team)
+	local player = team.player
 	local plateConfig = plateList[plateId]
 	local plate = plateConfig.model:Clone()
 	
 	if not plateConfig.spawned and not plateConfig.destroyed then
 		
+		plate:SetAttribute('Team', team.name)
 		plate.Name = plateConfig.plateName
-		plate.Parent = TycoonBasePlate
+		plate.Parent = baseplate.Parent
 		plate.Anchored = true
 		plateConfig.spawned = true
 		
-		local baseplatePart = TycoonBasePlate.BaseplateBuild
+		local baseplatePart = baseplate.BaseplateBuild
 		
-		local offsetY = baseplatePart.Size.Y / 2 + plate.Size.Y / 2
-		plate:PivotTo(baseplatePart.CFrame * plateConfig.position)
+		local offsetY = baseplatePart:GetPivot().Y + (baseplatePart.Size.Y / 2) + (plate.Size.Y / 2)
+		plate:PivotTo(CFrame.new(baseplatePart.CFrame.X,offsetY,baseplatePart.CFrame.Z) * plateConfig.position)
 		
 		local billboardGui = Instance.new('BillboardGui')
 		local textLabel = Instance.new('TextLabel')
@@ -41,32 +72,50 @@ local function spawn_plate(plateId)
 	
 	plate.Touched:Connect(function(other_part)
 		if not plateConfig.rebounce then
-			plateConfig.rebounce = true
 		
 			local character = other_part:FindFirstAncestorOfClass("Model")
-			local player = character and Players:GetPlayerFromCharacter(character)
+			local playerTouched = character and Players:GetPlayerFromCharacter(character)
 			
-			if player then
-				local eventPlatePressed = game.ServerScriptService.Events.PlatesPressedEvent
-				local message = {
-					plateType = plateConfig.plateType,
-					targetId = plateConfig.targetId,
-					plateId = plateId
-				}
+			if playerTouched then
+				local leaderstats = playerTouched:WaitForChild('leaderstats')
 				
-				eventPlatePressed:Fire(message)
-				plate:Destroy()
+				if leaderstats then
+					local balance = leaderstats:WaitForChild('Coins').Value
+					
+					if balance >= plateConfig.targetPrice then
+						
+						CoinsUtils.removeCoins(playerTouched, plateConfig.targetPrice)
+
+						if player.UserId == playerTouched.UserId then
+							plateConfig.rebounce = true
+
+							EventMessage(plateConfig, plateId, baseplate, team)
+
+							plate:Destroy()
+
+						end
+					else
+						print('Pas assez ! '.. plateConfig.targetPrice - balance)
+					
+					end
+					
+				end
 				
 			end
+		
 		end
 	end)
 end
 
 
-local plateSpawnEvent = game.ServerScriptService.Events.PlatesSpawnEvent
+local plateSpawnEvent = ServerScriptService.Events.Spawn.PlatesSpawn
 plateSpawnEvent.Event:Connect(function(message)
-	spawn_plate(message)
+	spawn_plate(message.plateId, message.baseplate, message.team)
 end)
 
-spawn_plate(1)
+local startPlateTouchedEvent = ServerScriptService.Events.StartPlateTouched
+
+startPlateTouchedEvent.Event:Connect(function(message)
+	spawn_plate(1, message.teamBaseplate, message.team)
+end)
 
